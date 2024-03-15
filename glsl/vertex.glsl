8aa2ca037121a246c8534e1a5dc219dd025b56a4
waitti <waitti@foxmail.com>
2024-03-15 23:10:06 +0800

    feat: add soil2

--- glsl/vertex.glsl
+++ glsl/vertex.glsl
@@ -7,8 +7,6 @@ uniform mat4 proj_matrix;
 uniform mat4 r_mat;
 
 void main(void) {
-    float x = gl_InstanceID * 2, y = 0, z = 0;
-    mat4 ins_trans_mat = mat4(1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, x, y, z, 1.0);
-    gl_Position = proj_matrix * mv_matrix * r_mat * ins_trans_mat * vec4(position, 1.0);
+    gl_Position = proj_matrix * mv_matrix * r_mat * vec4(position, 1.0);
     varyingColor = vec4(position, 1.0) * 0.5 + vec4(0.5, 0.5, 0.5, 0.5);
 }
--- src/Engine.h
+++ src/Engine.h
@@ -5,6 +5,7 @@
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
 #include "RenderingProgram.h"
+#include "third_party/SOIL2/src/SOIL2/SOIL2.h"
 #include "glm/glm.hpp"
 #include "glm/gtc/type_ptr.hpp"
 
@@ -23,6 +24,7 @@ class Engine {
 
     SetUpMat();
     SetUpVertices();
+    brick_texture = LoadTexture("../assets/brick.jpg");
   }
 
   void SetUpMat() {
@@ -57,6 +59,19 @@ class Engine {
     glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_positions), vertex_positions,
                  GL_STATIC_DRAW);
+
+    // texture
+    float tex[72];
+    for (int i = 0; i < 72; i += 6) {
+      tex[i] = 0.0f;
+      tex[i + 1] = 0.0f;
+      tex[i + 2] = 0.0f;
+      tex[i + 3] = 1.0f;
+      tex[i + 4] = 1.0f;
+      tex[i + 5] = 1.0f;
+    }
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(tex), tex, GL_STATIC_DRAW);
   }
 
   void Draw(double dt) {
@@ -71,9 +86,16 @@ class Engine {
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
     glEnableVertexAttribArray(0);
 
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(1);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, brick_texture);
+
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LEQUAL);
-    glDrawArraysInstanced(GL_TRIANGLES, 0, 36, 24);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
   }
 
   void UpdateMat(double dt) {
@@ -102,6 +124,16 @@ class Engine {
     GLuint loc =
         glGetUniformLocation(rendering_program_.program(), name.c_str());
     glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(value));
+  }
+
+  static GLuint LoadTexture(const char *path) {
+    GLuint texture_id = SOIL_load_OGL_texture(
+        path, SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
+    if (texture_id == 0) {
+      std::cout << "could not find texture file: " << path << std::endl;
+      exit(EXIT_FAILURE);
+    }
+    return texture_id;
   }
 
   void Clear() {
@@ -160,4 +192,6 @@ class Engine {
   glm::mat4 pMat, vMat, mMat, mvMat;
   glm::mat4 rMat;
   float thea;
+
+  GLuint brick_texture;
 };
